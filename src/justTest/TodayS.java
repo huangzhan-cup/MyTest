38398c93a5b2e210e7232fa5015675f48046f399
Tom <1134300263@qq.com>
2022-03-10 16:41:48 +0800

    second commit

--- src/justTest/TodayS.java
+++ src/justTest/TodayS.java
@@ -3,5 +3,7 @@ package justTest;
 public class TodayS {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
+
     }
 }
