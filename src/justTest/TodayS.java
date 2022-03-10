b2437e3917593463b7781571a02d9e773be86a0c
huangzhan-cup <55131992+huangzhan-cup@users.noreply.github.com>
2022-03-10 17:20:43 +0800

    pull commit

--- src/justTest/TodayS.java
+++ src/justTest/TodayS.java
@@ -4,6 +4,7 @@ public class TodayS {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("我在github上的一次修改，通过pull到ieda");
 
     }
 }
